5cfdbe5a4d705a29b2a1377bd9a64f341fc87fe9
Mefi-Boset Navarro Rivera <mefi-bosetnavarrorivera@MBPMB.local>
2018-09-18 07:36:51 -0600

    add folders models and db to folder server

---
+++ server/server.js
@@ -0,0 +1,25 @@
+
+var {mongoose} = require('./db/mongoose');
+
+// save new something
+
+
+
+/*
+var newTodo = new Todo({
+    text: '    Edit this video2   '//,
+    //complete: true,
+   // completedAt: 100
+})
+
+newTodo.save().then( (doc) => {
+
+    console.log('Saved todo',doc);
+
+}, (e) => {
+
+    console.log ('Unable to save todo');
+}
+
+);
+*/
