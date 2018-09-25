c93ca6805c650909c39941914f90e6cb112fbfdb
Mefi-Boset Navarro Rivera <mefi-bosetnavarrorivera@MBPMB.local>
2018-09-25 08:42:37 -0600

    add Post/todos with route and mongoose

--- server/server.js
+++ server/server.js
@@ -1,7 +1,35 @@
+const port = 3000;
+
+var express = require ('express');
+var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
+var {Todo} = require ('./models/todo');
+var {User} = require ('./models/user');
 
-// save new something
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res) => {
+
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then(
+        (doc) => {
+            res.send(doc);
+        },
+        (e) => {
+            res.status(400).send(e);
+        }
+    );
+});
+
+app.listen(port, () => {
+    console.log('Started on port ' + port);
+});
 
 
 
